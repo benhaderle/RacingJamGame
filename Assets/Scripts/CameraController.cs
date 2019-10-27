c959db4ddb4ce175a27db9c5d37d9c448441bf82
Ben Haderle <benhaderle@gmail.com>
2019-10-27 13:50:33 -0700

    more work on mergeing and player movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject car;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.LookAt(car.transform.position);
+        transform.position = car.transform.position + new Vector3(0, 4, -6); ;// new Vector3(0, 4, -6);
+    }
+}
