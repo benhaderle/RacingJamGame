3e2036593c83f0faac6b1d339cfa91ade031c993
Ben Haderle <benhaderle@gmail.com>
2019-10-26 17:44:12 -0700

    track gen done

---
+++ Assets/Scripts/TestCube.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray r = new Ray(transform.position, -transform.up);
+        GetComponent<Collider>().Raycast(r, out hit, Mathf.Infinity);
+        Debug.Log(GetComponent<Collider>().Raycast(r, out hit, Mathf.Infinity));
+        if (Physics.Raycast(r, out hit, Mathf.Infinity)) {
+            Debug.Log("hit");
+            float rotation = Vector3.Angle(transform.up, hit.normal);
+            transform.Rotate(transform.right, rotation);
+        }
+    }
+}
